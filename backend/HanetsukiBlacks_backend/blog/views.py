7c93a60c140475558f8628e7053fcb40cab10635
show-creator <git>
2024-04-03 00:58:00 +0900

    修正

--- backend/HanetsukiBlacks_backend/blog/views.py
+++ backend/HanetsukiBlacks_backend/blog/views.py
@@ -6,7 +6,10 @@ class BlogPostViewSet(viewsets.ModelViewSet):
     queryset = Blog.objects.order_by('-blog_time')
     serializer_class = BlogPostSerializer
     
-class BlogLatest(viewsets.ModelViewSet):
-    queryset = Blog.objects.order_by('-blog_time').first()
+class BlogLatest(viewsets.ReadOnlyModelViewSet):
     serializer_class = BlogPostSerializer
+    def get_queryset(self):
+        # 最新のブログポストだけを含むクエリセットを返します
+        return Blog.objects.order_by('-blog_time')[:1]
+    
 
