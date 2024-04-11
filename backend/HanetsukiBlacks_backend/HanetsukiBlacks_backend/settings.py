7a81a8527f43c04f6a7ffe7297b0f49eef2ecde3
show-creator <git>
2024-04-11 19:29:41 +0900

    修正34

--- backend/HanetsukiBlacks_backend/HanetsukiBlacks_backend/settings.py
+++ backend/HanetsukiBlacks_backend/HanetsukiBlacks_backend/settings.py
@@ -168,7 +168,7 @@ STATIC_URL = 'static/'
 
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOW_ORIGINS = [
+CORS_ALLOWED_ORIGINS = [
     "https://hanetsukiblacks-wtgx.onrender.com/",
     "https://hntkblacks.cloudfree.jp"
 ]
